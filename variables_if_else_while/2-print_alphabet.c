44cc2f3dceb5cef31afdcf7783c4f3bae789f866
Jeffrey Rodrigo <jeffreyfloresg@gmail.com>
2023-02-23 10:24:49 -0600

    task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lcl;
+
+	for (lcl = 'a'; lcl <= 'z'; ++lcl)
+		putchar(lcl);
+	putchar(\n);
+	return (0);
+}
