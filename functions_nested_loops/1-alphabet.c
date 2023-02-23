fe62c362a14e26e6c88af403e50b05af2fb1314f
Jeffrey Rodrigo <jeffreyfloresg@gmail.com>
2023-02-23 14:33:15 -0600

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void);
+{
+	char lcl;
+
+	for (lcl = 'a'; lcl <= 'z'; ++lcl)
+		_printchar(lcl);
+	_printchar('\n')
+	return (0);
+}
