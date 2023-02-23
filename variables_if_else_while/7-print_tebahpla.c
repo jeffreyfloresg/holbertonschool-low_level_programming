3dfec714236af838f809892df75e6c8086129435
Jeffrey Rodrigo <jeffreyfloresg@gmail.com>
2023-02-23 11:08:50 -0600

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lcl;
+
+	for (lcl = 'z'; lcl <= 'a'; --lcl)
+		putchar(lcl);
+	putchar('\n');
+	return (0);
+}
