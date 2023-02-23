92fa3c96bb06bc73c7c3e09817807b13779c92af
Jeffrey Rodrigo <jeffreyfloresg@gmail.com>
2023-02-23 17:25:23 -0600

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - prints 1 if argument is lowercase
+ *	      prints 0 otherwise
+ * @c is one character
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	int iTA;
+
+	iTA = c + 32;
+	if (iTA < 97)
+		return (1);
+	else
+		return (0);
+}
