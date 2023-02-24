367705a02f1abe614b71f80a72b40ae22a69e2cd
Jeffrey Rodrigo <jeffreyfloresg@gmail.com>
2023-02-23 21:50:32 -0600

    task 11 fix 3

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,20 +10,22 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; ++n)
+		for (; n < 98; ++n)
 		{
 			printf("%d", n);
 			printf(", ");
 		}
+		printf("\n");
 		if (n == 98)
 			printf("%d", n);
 	}
-	if (n > 98)
+	else if (n > 98)
 	{
 		for (; n >= 98; --n)
 		{
 			printf("%d", n);
 			printf(", ");
 		}
+		printf("\n")
 	}
 }
