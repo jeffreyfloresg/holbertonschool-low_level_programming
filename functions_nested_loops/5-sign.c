5025baa8a76d2123d226ef257517932c86339a79
Jeffrey Rodrigo <jeffreyfloresg@gmail.com>
2023-02-23 19:03:21 -0600

    task 5 fix 1

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,13 +13,19 @@ int print_sign(int n)
 	int result;
 
 	if (n > 0)
+	{
 		_putchar('+');
 		result = 1;
+	}
 	else if (n == 0)
+	{
 		_putchar('0');
 		result = 0;
+	}
 	else
+	{
 		_putchar('-');
 		result = -1;
+	}
 	return (result);
 }
