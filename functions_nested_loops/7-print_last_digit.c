7762bc00eaf1f7e30c728e0882b27d1b60fb76de
Jeffrey Rodrigo <jeffreyfloresg@gmail.com>
2023-02-23 19:35:27 -0600

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of integer
+ * @n: is an integer
+ * Return: 0.
+ */
+
+int print_last_digit(int n);
+{
+	int r;
+
+	r = n % 10;
+	_putchar(r);
+	return (r);
+}
