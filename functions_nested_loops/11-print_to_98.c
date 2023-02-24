518b120ed103708a1aadcae431d3c6da67563372
Jeffrey Rodrigo <jeffreyfloresg@gmail.com>
2023-02-23 21:23:55 -0600

    task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints every number from n to 98
+ * @n: is an integer
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n < 98; ++n)
+		{
+			printf("%d", n);
+			printf(", ");
+		}
+		if (n == 98)
+			printf("%d", n);
+	}
+	if (n > 98)
+	{
+		for (; n > 98; --n)
+		{
+			printf("%d", n);
+			printf(", ");
+		}
+}
