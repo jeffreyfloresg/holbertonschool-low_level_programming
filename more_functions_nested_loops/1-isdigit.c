7d6f6c75666089dca6d7b989270bcb9e822f9761
Jeffrey Rodrigo <jeffreyfloresg@gmail.com>
2023-02-24 18:26:44 -0600

    task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if c is uppercase char.
+ * @c: is a character
+ * Return: 1 if c is uppercase,
+ *         0 if it is not uppercase.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 int _putchar(int c);
 int _isupper(char c);
+int _isdigit(int c);
 #endif
