1ac47ac0fcc48c418d8c8690d9cf80f17b7cc7ee
Jeffrey Rodrigo <jeffreyfloresg@gmail.com>
2023-02-24 20:03:54 -0600

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ * 			except 2 and 4.
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= 9; ++i)
+	{
+		if (i == '2' || i == 5)
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
